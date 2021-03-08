80c7121a3115ebf7eb86432bcbfd6cb158703f1c
Mj Octavio <mj_octaviojr@yahoo.com>
2021-03-09 09:47:49 +1100

    Added functionality on TextBoxPassword btnClearText

--- octalibrary/src/main/java/com/programm3r/octalibrary/TextBoxPassword.kt
+++ octalibrary/src/main/java/com/programm3r/octalibrary/TextBoxPassword.kt
@@ -2,10 +2,19 @@ package com.programm3r.octalibrary
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.RelativeLayout
 
-class TextBoxPassword: RelativeLayout {
+class TextBoxPassword: RelativeLayout, View.OnClickListener {
+
+    private lateinit var txtPassword:   EditText
+    private lateinit var  btnShowText:  Button
+    private lateinit var  btnClearText: Button
+
 
     constructor(context: Context) : super(context) {
         initializeViews(context)
@@ -20,10 +29,38 @@ class TextBoxPassword: RelativeLayout {
     private fun initializeViews(context: Context) {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         inflater.inflate(R.layout.text_box_password, this)
+
+        txtPassword = findViewById<EditText>(R.id.txtPassword)
+        btnShowText = findViewById<Button>(R.id.btnShowText)
+        btnClearText = findViewById<Button>(R.id.btnClearText)
+
+        btnShowText.setOnClickListener(this)
+        btnClearText.setOnClickListener(this)
+
     }
 
     protected override fun onFinishInflate() {
         super.onFinishInflate()
     }
+
+    override fun onClick(view: View?) {
+
+        when (view?.id) {
+
+            R.id.btnShowText -> {
+
+            }
+
+            R.id.btnClearText -> {
+
+                txtPassword.setText("")
+
+            }
+
+        }
+
+
+    }
+
 
 }
